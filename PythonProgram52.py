c0c6b249281c98e558b88533cace41416b985454
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-08-04 14:33:06 +0600

    Create PythonProgram52.py

---
+++ PythonProgram52.py
@@ -0,0 +1,12 @@
+#Exception Handling part 1
+
+try:
+    list = [20,0,30]
+    result = list[0] / list[3]
+    print(result)
+    print("Done")
+except ZeroDivisionError:
+    print("Dividing by zero is not possible")
+except IndexError:
+    print("Index error")
+    print("Successful")
